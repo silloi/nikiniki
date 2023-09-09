75f119be90da83c4bef1786e474c9f52c988b271
silloi <silloi1993@gmail.com>
2023-09-09 19:25:08 +0900

    create post form

--- src/routes/times/+page.server.ts
+++ src/routes/times/+page.server.ts
@@ -1,28 +1,28 @@
 import { error, fail } from '@sveltejs/kit';
 
 export const actions = {
-	createTime: async ({ request, locals: { supabase, getSession } }) => {
+	createPost: async ({ request, locals: { supabase, getSession } }) => {
 		const session = await getSession();
 
 		if (!session) {
 			// the user is not signed in
 			throw error(401, { message: 'Unauthorized' });
 		}
-		// we are save, let the user create the time
+		// we are save, let the user create the post
 		const formData = await request.formData();
 		const content = formData.get('content') as string;
 
-		const { error: createTimeError, data: newTime } = await supabase
+		const { error: createPostError, data: newPost } = await supabase
 			.from('times')
 			.insert({ user_id: session.user.id, content });
 
-		if (createTimeError) {
+		if (createPostError) {
 			return fail(500, {
-				supabaseErrorMessage: createTimeError.message
+				supabaseErrorMessage: createPostError.message
 			});
 		}
 		return {
-			newTime
+			newPost
 		};
 	}
 };
--- src/routes/times/+page.svelte
+++ src/routes/times/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	export let data;
+
+	let content = '';
 
 	let loadedData: any[] | null = [];
 	async function loadData() {
@@ -11,6 +15,11 @@
 		loadData();
 	}
 </script>
+
+<form method="POST" action="?/createPost" use:enhance>
+	<input type="text" name="content" bind:value={content} />
+	<button>Post</button>
+</form>
 
 {#if data.session}
 	<p>client-side data fetching with RLS</p>
